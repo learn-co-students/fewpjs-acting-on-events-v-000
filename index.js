999c89aadf44b7e70a7616dbd3c2a0b599226140
Gingertonic <bethmschofield@gmail.com>
2019-11-28 10:44:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 // Your code here
 const DODGER = document.querySelector("#dodger")
-let DODGERLEFT = parseInt(DODGER.style.left)
 
 
 function moveDodgerLeft() {
-    const newPosition = DODGERLEFT - 1
-    DODGER.style.left = `${newPosition}px`
+    const newPosition = parseInt(DODGER.style.left.replace('px', '')) - 10
+    if (newPosition >= 0){
+        DODGER.style.left = `${newPosition}px`
+    }
 }
 
 function moveDodgerRight() {
-    const newPosition = DODGERLEFT + 1
-    DODGER.style.left = `${newPosition}px`
+    const newPosition = parseInt(DODGER.style.left.replace('px', '')) + 10
+    if (newPosition <= 360){
+        DODGER.style.left = `${newPosition}px`
+    }
+}
+
+
+document.addEventListener("keydown", e => handleKeydown(e))
+
+const handleKeydown = e => {
+    if (e.key == "ArrowLeft"){
+        moveDodgerLeft();
+    } else if (e.key == "ArrowRight") {
+        moveDodgerRight();
+    }
 }
