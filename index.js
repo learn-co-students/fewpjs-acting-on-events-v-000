0852a8c76be4023c77b7f697a911bc61d9b84a9f
Gingertonic <bethmschofield@gmail.com>
2019-11-28 10:30:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+const DODGER = document.querySelector("#dodger")
+let DODGERLEFT = parseInt(DODGER.style.left)
+
+
+function moveDodgerLeft() {
+    const newPosition = DODGERLEFT - 1
+    DODGER.style.left = `${newPosition}px`
+}
+
+function moveDodgerRight() {
+    const newPosition = DODGERLEFT + 1
+    DODGER.style.left = `${newPosition}px`
+}
