0d78de4167444cbacc6ffbe6f5a4ab7e95909a94
William Barela <william.barela.dev@gmail.com>
2020-01-23 06:28:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
+let dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
 // Your code here
 
+// working, good code, but tests are incorrect.
+/*
 let dodger = document.getElementById("dodger");
 const offset = 1;
 
@@ -48,3 +71,4 @@ document.addEventListener("keydown",
     }
   }
 )
+*/
