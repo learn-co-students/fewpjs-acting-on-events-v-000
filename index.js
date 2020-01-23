2387ccbf6aaccc54675611a9c9f155fd4d989075
William Barela <william.barela.dev@gmail.com>
2020-01-23 06:23:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Your code here
+
+let dodger = document.getElementById("dodger");
+const offset = 1;
+
+function leftInt (dodger){
+  let leftString = dodger.style.left.replace("px","");
+  return parseInt(leftString, 10);
+}
+
+function getRightGameBoundary(){
+  let gameRightBoundary = document.getElementById("game");
+  
+  return gameRightBoundary.offsetWidth;
+}
+
+function moveDodgerLeft(){
+  let left = leftInt(dodger);
+  
+  if (left > 0){
+    dodger.style.left = `${left - offset}px`;
+  }
+}
+
+function moveDodgerRight(){
+  let left = leftInt(dodger);
+  let gameRightBoundary = getRightGameBoundary();
+  console.log(left);
+  
+  if(left < (gameRightBoundary - dodger.offsetWidth)){
+    dodger.style.left = `${left + offset}px`;
+  }
+  
+}
+
+document.addEventListener("keydown", 
+  function(e){
+    if (e.key === "ArrowLeft"){
+      moveDodgerLeft();
+    }
+  }
+)
+
+document.addEventListener("keydown", 
+  function(e){
+    if (e.key === "ArrowRight"){
+      moveDodgerRight();
+    }
+  }
+)
