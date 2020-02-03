db68e4200b52ccb9f74824fd50cc54aec649d600
danetter17 <etter.dt@gmail.com>
2020-02-02 18:03:41 -0600

    Add extra event listeners, allows user to move dodger up and down as well as left and right

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 let dodger = document.getElementById('dodger');
+dodger.style.backgroundColor = "#FF69B4";
+
 
 function moveDodgerLeft() {
   let leftNumbers = dodger.style.left.replace('px', '');
@@ -11,19 +13,37 @@ function moveDodgerLeft() {
 function moveDodgerRight() {
   let leftNumbers = dodger.style.left.replace('px', '');
   let left = parseInt(leftNumbers, 10)
-  if (left > 0) {
+  if (left < 360) {
     dodger.style.left = `${left + 1}px`
   }
 }
 
-document.addEventListener('keydown', function(e) {
-  if (e.key === "ArrowLeft") {
-    moveDodgerLeft()
-  }
-})
+function moveDodgerUp() {
+  let bottomNumbers = dodger.style.bottom.replace("px", "");
+  let bottom = parseInt(bottomNumbers, 10);
 
-document.addEventListener('keydown', function(e) {
-  if (e.key === "ArrowRight") {
-    moveDodgerRight()
+  if (bottom < 380) {
+    dodger.style.bottom = `${bottom + 1}px`;
   }
-})
+}
+
+function moveDodgerDown() {
+  let bottomNumbers = dodger.style.bottom.replace("px", "");
+  let bottom = parseInt(bottomNumbers, 10);
+
+  if (bottom > 0) {
+    dodger.style.bottom = `${bottom - 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  } else if (e.key === "ArrowDown") {
+    moveDodgerDown();
+  } else {
+    moveDodgerUp();
+  }
+});
