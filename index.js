dc706f0db62187f90b564e3ffe62ecd904b1a794
Toni-Ty <antonique2479@yahoo.com>
2020-04-20 07:40:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-function preventRefreshOnSubmit(){
-    document.querySelector('form').addEventListener('submit', function(event){
-      event.preventDefault()
-    })
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (e.key === "ArrowRight") {
+    moveDodgerLeft();
+  }
+});
+
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
 }
 
-preventRefreshOnSubmit()
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
