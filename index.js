cb16e7825a804526819a2f55010e3e2304f4e3cb
Eric Johnson <ericjohnson0216@icloud.com>
2020-08-21 22:26:17 -0400

    Done.

--- index.js
+++ index.js
@@ -32,10 +32,10 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerRight() {
-    let rightNumbers = dodger.style.right.replace("px", "");
-    let right = parseInt(rightNumbers, 10);
+    let rightNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(rightNumbers, 10);
 
-    if (right > 179) {
-        dodger.style.right = `${left + 1}px`;
+    if (left > 0) {
+        dodger.style.left = `${left + 1}px`;
     }
 }
