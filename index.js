e36d7d09154d3a0ff35a4be1958d04f3ac462246
Eric Johnson <ericjohnson0216@icloud.com>
2020-08-21 22:18:41 -0400

    Done.

--- index.js
+++ index.js
@@ -36,6 +36,6 @@ function moveDodgerRight() {
     let right = parseInt(rightNumbers, 10);
 
     if (right > 179) {
-        dodger.style.right = `${right + 1}px`;
+        dodger.style.right = `${left + 1}px`;
     }
 }
